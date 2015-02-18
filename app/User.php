57a52ba46f91a65872269fd6933489d11a363506
Alexander Tash <atash@rxsavingsllc.com>
2015-02-18 20:27:42 +0000

    Basic setup

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'email', 'password'];
+	protected $fillable = ['alias', 'email', 'password'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
